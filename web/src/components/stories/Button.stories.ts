6ecc68dcd1b3dd13bc8f427c28d0c46dac43d26f
MrJackdaw <MrJackdaw@users.noreply.github.com>
2023-04-05 13:58:12 -0700

    * code cleanup

--- web/src/components/stories/Button.stories.ts
+++ web/src/components/stories/Button.stories.ts
@@ -8,7 +8,11 @@ const meta = {
   tags: ["autodocs"],
   /** Controls for changing/previewing component props */
   argTypes: {
-    size: { control: "select", options: ["sm", "md", "lg"] }
+    size: { control: "radio", options: ["sm", "md", "lg"], defaultValue: "md" },
+    variant: {
+      control: "radio",
+      options: [undefined, "accent", "outlined", "transparent"]
+    }
   },
   /** Default props for all instances */
   args: {
@@ -20,26 +24,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const Main: Story = { args: {} };
-
-export const Accent: Story = {
-  args: { variant: "accent" }
-};
-
-export const Outlined: Story = {
-  args: { variant: "outlined" }
-};
+export const Default: Story = { args: { size: "md" } };
 
 export const Round: Story = { args: { round: true, children: "i" } };
-
-export const Large: Story = {
-  args: { size: "lg" }
-};
-
-export const Medium: Story = {
-  args: { size: "md" }
-};
-
-export const Small: Story = {
-  args: { size: "sm" }
-};
